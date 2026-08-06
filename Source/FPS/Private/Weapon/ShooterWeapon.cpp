4bf025573f41ead62cc540d40232f119375b4d1b
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-08-07 00:31:40 +0530

    Setup Shooter Weapon

---
+++ Source/FPS/Private/Weapon/ShooterWeapon.cpp
@@ -0,0 +1,30 @@
+﻿// No Copyright.
+
+#include "Weapon/ShooterWeapon.h"
+
+AShooterWeapon::AShooterWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	SetReplicates(true);
+	bNetUseOwnerRelevancy = true;
+	
+	WeaponMesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh1P"));
+	SetRootComponent(WeaponMesh1P);
+	WeaponMesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	WeaponMesh1P->SetCastShadow(false);
+	WeaponMesh1P->SetReceivesDecals(false);
+	WeaponMesh1P->SetHiddenInGame(true);
+	
+	WeaponMesh3P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh3P"));
+	WeaponMesh3P->SetupAttachment(GetRootComponent());
+	WeaponMesh3P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	WeaponMesh3P->SetCastShadow(true);
+	WeaponMesh3P->SetReceivesDecals(false);
+	WeaponMesh3P->SetHiddenInGame(true);
+}
+
+void AShooterWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/FPS/Public/Weapon/ShooterWeapon.h
@@ -0,0 +1,35 @@
+﻿// No Copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ShooterWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API AShooterWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AShooterWeapon();
+	
+	FORCEINLINE USkeletalMeshComponent* GetWeaponMesh1P() const { return WeaponMesh1P; }
+	
+	FORCEINLINE USkeletalMeshComponent* GetWeaponMesh3P() const { return WeaponMesh3P; }
+
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	// Weapon mesh: 1st person view.
+	UPROPERTY(VisibleAnywhere, Category = "FPS|Weapon")
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh1P;
+	
+	// Weapon mesh: 3rd person view.
+	UPROPERTY(VisibleAnywhere, Category = "FPS|Weapon")
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh3P;
+};
