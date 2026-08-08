1dcfdaabc0668b4a1352444adf376808c4ae6c98
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-08-08 16:24:39 +0530

    Setup Shooter Weapon Data

---
+++ Source/FPS/Private/Weapon/ShooterWeapon_DataAsset.cpp
@@ -0,0 +1,4 @@
+﻿// No Copyright.
+
+#include "Weapon/ShooterWeapon_DataAsset.h"
+
---
+++ Source/FPS/Public/Weapon/ShooterWeapon_DataAsset.h
@@ -0,0 +1,22 @@
+﻿// No Copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "ShooterWeapon_DataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API UShooterWeapon_DataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,
+		Category = "FPS|WeaponData|Weapons", meta = (Categories = "Weapon.WeaponType"))
+	TMap<FGameplayTag, FName> WeaponGripPoints;
+};
